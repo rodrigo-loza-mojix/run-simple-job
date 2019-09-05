9c5d27de42ccbbdf47748e0453db5c0a762403c6
Rodrigo Loza <rodrigo.loza@mojix.com>
2019-09-05 15:38:37 -0400

    indentation

--- src/run-job.sh
+++ src/run-job.sh
@@ -3,11 +3,11 @@ log_level=${LOG_LEVEL:=INFO}
 streamapps=(rulesprocessor mongoinjector actionprocessor transformbridge hbridge reportgenerator m2k k2m)
 
 for streamapp in ${streamapps[@]}; do
-	echo "Updating ${streamapp} log level to ${log_level}"
+  echo "Updating ${streamapp} log level to ${log_level}"
   curl -H 'Content-type: text/plain' http://"${streamapp}":8000/config/log -X POST -d "com.tierconnect=${log_level}"
   if [[ $? -ne 0 ]]; then
     echo "Streamapp ${streamapp} failed to update its log level to ${log_level}"
   fi;
-	echo ""
+  echo ""
 done;
 
