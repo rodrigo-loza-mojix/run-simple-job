d7f8e01bd26bb27f13a2397b82ae9352692764e0
Rodrigo Loza <rodrigo.loza@mojix.com>
2020-02-03 15:56:20 -0400

    * get-mojix-hubs

--- src/run-job.sh
+++ src/run-job.sh
@@ -1,14 +1,13 @@
 # !/bin/bash
-url="https://red.vizix.io/statemachine-api-configuration/rest/configuration/locations/?level=premise&size=500"
+url="https://red.vizix.io/statemachine-api-configuration/rest/configuration/hub"
 apikey="CBTP0IEXSMRDRSEE"
-filespath="/opt/options/premise_codes/"
+filespath="/opt/options/hub_ids/"
 
 while [[ true ]]; do
-  # Retrieve codes and names.
-  codes=($(curl -X GET -H "apikey: ${apikey}" ${url} | jq -r ".[].code"))
-  names=($(curl -X GET -H "apikey: ${apikey}" ${url} | jq -r ".[].name" | tr " " "_"))
+  # Retrieve hubs.
+  hubs=($(curl -X GET -H "apikey: ${apikey}" ${url} | jq -r ".[].id"))
   
-  # Create codes and names based on retrieved input.
+  # Create hubs based on retrieved input.
   are_there_files=$(ls ${filespath} | wc -l)
   echo "Are there files in ${filespath} : " ${are_there_files}
   if [[ "${are_there_files}" -gt 0 ]]; then
@@ -18,8 +17,8 @@ while [[ true ]]; do
   are_there_files=$(ls ${filespath} | wc -l)
   echo "Are there files in ${filespath} : " ${are_there_files}
   echo "Updating filenames in ${filespath}"
-  for ((index=0; index<"${#codes[@]}"; index++)); do
-      filename="${names[${index}]}___${codes[${index}]}"
+  for ((index=0; index<"${#hubs[@]}"; index++)); do
+      filename="${hubs[${index}]}"
       touch "${filespath}"/"${filename}"
   done;
   are_there_files=$(ls ${filespath} | wc -l)
